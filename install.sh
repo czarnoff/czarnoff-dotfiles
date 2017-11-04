d7b097bd46a33c6e69b4cf7cab7ec568082d26df
Jeffery Williams <jeffery@jawmark.net>
2017-11-03 19:25:32 -0500

    Use i3 and ranger

--- install.sh
+++ install.sh
@@ -14,7 +14,24 @@ do
    fi
 done
 
+pushd .
+cd config
+for x in *
+do
+   if [ -n $x -a "$x" != ".git" ]
+   then
+      echo $x
+      rm -rf "$HOME/.config/$x"
+      ln -s "`pwd`/$x" ~/.config/
+   fi
+done
+
+popd
+
 rm -rf ~/bin
 ln -s "`pwd`/bin" ~/
 rm -rf ~/.oh-my-zsh
 ln -s "`pwd`/oh-my-zsh" ~/.oh-my-zsh
+
+#sudo pip3 install pywal
+#sudo pip3 install wal-steam
