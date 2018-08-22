e78582ab43e3e154dc2f5558a13ac6d3a136fc51
Jeffery Williams <jeffery@jawmark.net>
2018-08-22 11:39:28 -0500

    Allow synergy to run as a client or a server from the same command.

--- bin/start_syn.sh
+++ bin/start_syn.sh
@@ -1,13 +1,30 @@
 #! /bin/bash
-if pgrep synergys
+
+HOST="gallium-zeppelin"
+if pgrep synergys || pgrep synergyc
 then
     echo "Synergy running.  Stop first."
 else
     echo "Starting synergy"
-    XAUTHORITY=$HOME/.Xauthority ; export XAUTHORITY
-    env_keep="DISPLAY XAUTHORITY"
-    synergys -c ~/.quicksynergy/synergy.conf
-    #synergys
-fi
+
+    case $1 in
+        "Client"| "c" | "-c")
+            if [ -n "$2" ]
+            then 
+                HOST=$2
+            fi
+            echo "Client of $HOST"
+            synergyc $HOST
+        ;;
+
+         *)
+             echo "Starting Server"
+             XAUTHORITY=$HOME/.Xauthority ; export XAUTHORITY
+             env_keep="DISPLAY XAUTHORITY"
+             synergys -c ~/.quicksynergy/synergy.conf
+             #synergys
+             ;;
+     esac
+ fi
 
 #sudo service synergy start
--- bin/stop_syn.sh
+++ bin/stop_syn.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 if pgrep synergys
 then
+   echo "Killing server"
    #sudo killall synergys
    killall synergys
+   sleep 1
+   killall -9 synergys
 else
-   echo "cannot find synergy"
+   if pgrep synergyc
+   then
+       echo "Killing client"
+       killall synergyc
+       sleep 1
+       killall -9 synergyc
+       exit 0
+   else
+       echo "Cannot find synergy"
+   fi
 fi
 #sudo service synergy stop
