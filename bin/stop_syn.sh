5e92f448d67dbc2d0b02cfb413093fa164232be6
Jeffery Williams <jeffery@jawmark.net>
2018-10-15 23:52:06 -0500

    Make a function of killing synergy types.

--- bin/stop_syn.sh
+++ bin/stop_syn.sh
@@ -1,19 +1,25 @@
 #! /bin/bash
+
+THING="monster"
+
+
+function syn_kill {
+   echo -n "Killing $THING... "
+   #sudo killall synergys
+   killall $THING && echo "Dead"
+   sleep 1
+   killall -9 $THING >> /dev/null 2>&1 && echo "Dead"
+   exit 0
+}
 if pgrep synergys
 then
-   echo "Killing server"
-   #sudo killall synergys
-   killall synergys
-   sleep 1
-   killall -9 synergys
+    THING="synergys"
+    syn_kill
 else
    if pgrep synergyc
    then
-       echo "Killing client"
-       killall synergyc
-       sleep 1
-       killall -9 synergyc
-       exit 0
+      THING="synergyc"
+      syn_kill
    else
        echo "Cannot find synergy"
    fi
