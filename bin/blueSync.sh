3e1a7f3a5dcac0cc9d908933c57c893e0236e5aa
Jeffery Williams <jeffery@jawmark.net>
2019-01-09 20:44:13 -0500

    Set volume and connect to bluetooth automatically

--- bin/blueSync.sh
+++ bin/blueSync.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo  connect FC:58:FA:91:9F:BA | bluetoothctl
+
+sleep 1
+
 BLUEZNUM=`pactl list short cards | grep bluez_card | cut -f1 `
 BLUEZCARD=`pactl list short sinks | grep bluez_sink | cut -f2 `
 
@@ -10,3 +14,5 @@ sleep 0.5
 pactl set-card-profile $BLUEZNUM a2dp_sink
 pactl set-default-sink $BLUEZCARD
 pactl set-sink-mute $BLUEZCARD 0
+sleep 0.5
+pactl set-sink-volume $BLUEZCARD 35000
