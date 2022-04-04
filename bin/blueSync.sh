e4468ea1d51a2ae3d1eba998e5da65103b0ed6b0
Jeffery Williams <jeffery@jawmark.net>
2022-04-04 17:06:54 -0400

    Use any bluetooth thing we find

--- bin/blueSync.sh
+++ bin/blueSync.sh
@@ -13,6 +13,11 @@ pactl set-card-profile $BLUEZNUM headset_head_unit
 sleep 0.5
 pactl set-card-profile $BLUEZNUM a2dp_sink
 pactl set-default-sink $BLUEZCARD
+for x in `pactl list sink-inputs | grep Sink | grep Input | cut -f2 -d'#'`
+do 
+    echo $x
+    pacmd move-sink-input $x $BLUEZCARD
+done
 pactl set-sink-mute $BLUEZCARD 0
 sleep 0.5
 pactl set-sink-volume $BLUEZCARD 35000
