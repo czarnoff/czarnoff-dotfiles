bef6cde56e414bdee2b2e0bbc046f5a4049afb41
Jeffery Williams <jeffery@jawmark.net>
2022-04-04 17:00:24 -0400

    Disable the touchscreen and the touchpad

--- bin/fix_mouse.sh
+++ bin/fix_mouse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT=$(xinput | grep -i "synaptics" )
+INPUT=$(xinput | grep -i -e "synaptics" -e "touchscreen")
 echo $INPUT
 
 
@@ -11,6 +11,7 @@ then
       case $x in
          id*)
             xinput set-prop $(echo $x | cut -d= -f2) "Device Enabled" 0
+            echo "Disabled $x"
             ;;
          *)
             ;;
