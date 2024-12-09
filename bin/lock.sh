bdb280b8a97bb337c87596d221074721737e521d
Jeffery Williams <jeffery@jawmark.net>
2024-12-09 01:37:38 -0500

    Allow hybernation.

--- bin/lock.sh
+++ bin/lock.sh
@@ -23,14 +23,24 @@ if [ -n "$1" ]
 then
     fn-f7-emergency.sh
 
-    notify-send -t 500 "Lock" "Suspending"
+    if [ "$1" == "h" ]
+    then
+      notify-send -t 500 "Lock" "hibernating"
+    else
+      notify-send -t 500 "Lock" "Suspending"
+    fi
     stop_syn.sh
     sleep 0.5 
     echo 'restart()' | qtile shell
     nmcli radio wifi on
     notify-send -t 500 "Lock" "Enabling wifi"
     sleep 2
-    systemctl suspend
+    if [ "$1" == "h" ]
+    then
+      systemctl hibernate
+    else
+      systemctl suspend
+    fi
     asdf
 fi
 
@@ -39,11 +49,11 @@ scrot /tmp/screen.png
 
 notify-send -t 500 "Lock" "Locking screen."
 
-#convert /tmp/screen.png -paint 3 -modulate 80 /tmp/screen.png
-convert /tmp/screen.png -paint 3 /tmp/screen.png
-#convert /tmp/screen.png -emboss 3 /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+#magick /tmp/screen.png -paint 3 -modulate 80 /tmp/screen.png
+magick /tmp/screen.png -paint 3 /tmp/screen.png
+#magick /tmp/screen.png -emboss 3 /tmp/screen.png
+magick /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+magick /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
 #mocp -P
 
 locker &
